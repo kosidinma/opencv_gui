6b04d667abe3a1acae02795be6bda423f024cf86
kosidinma <kosynom@yahoo.com>
2017-02-22 17:09:19 +0100

    Added facial recognition features

--- opencv_gui/MyForm.cpp
+++ opencv_gui/MyForm.cpp
@@ -1,29 +1,39 @@
-#include "MyForm.h"
-#include "opencv2/imgproc.hpp"
-#include "opencv2/imgcodecs.hpp"
-#include "opencv2/highgui.hpp"
-
+#include "MyForm.h"  //has all the cv libraries added
+#include <iostream>
+#include <stdio.h>
 
 using namespace System;
 using namespace System::Windows::Forms;
 using namespace cv;
 
-//opencv stuff i added
-/**
-* @file Morphology_1.cpp
-* @brief Erosion and Dilation sample code
-* @author OpenCV team
-*/
 
 /// Global variables
-Mat src, erosion_dst, dilation_dst;
+/*
+The classification will be done using the LBP trained classifier for the face 
+and Haar for the eyes (LBP has no eye training set).
 
-int erosion_elem = 0;
-int erosion_size = 0;
-int dilation_elem = 0;
-int dilation_size = 0;
-int const max_elem = 2;
-int const max_kernel_size = 21;
+Trade-off (Speed vs Accuracy)
+LBP is faster (a few times faster) but less accurate. (10-20% less than Haar).
+E.g. If you want to detect faces on an embedded system, LBP is the choice, because it does all the calculations in integers. 
+Haar uses floats, whick is a killer for embedded/mobile.
+
+An LBP cascade can still be trained to perform similarly (or better) than the Haar cascade, but out of the box, 
+the Haar cascade is about 3x slower better at accurately detecting the location of a face. 
+This increase in accuracy is quite significant given that face detection can operate in the 95%+ accuracy range.
+
+Chose LBP because processing speed is important for timing GUI applications such as this
+*/
+
+//std::string face_cascade_name = "../data/facial_recog_classifiers/lbpcascade_frontalface.xml"; //face classifier file
+std::string face_cascade_name = "../data/facial_recog_classifiers/haarcascade_frontalface_alt.xml"; //face classifier file
+std::string eyes_cascade_name = "../data/facial_recog_classifiers/haarcascade_eye_tree_eyeglasses.xml"; //eye classifier file
+//initialize the classifiers
+CascadeClassifier face_cascade; 
+CascadeClassifier eyes_cascade;
+bool face_detected = false;
+int brightness_val;
+int contrast_val;
+
 
 
 [STAThread]
@@ -37,90 +47,168 @@ void Main(array<opencv_gui::String^>^ args)
 
 
 
-
-int opencv_gui::button_test(void)
+void DrawCvImage(System::Windows::Forms::Control^ control, cv::Mat& colorImage)
 {
-	/// Load an image
-	src = imread("../data/picture001.jpg", IMREAD_COLOR);
+	System::Drawing::Graphics^ graphics = control->CreateGraphics();
+	System::IntPtr ptr(colorImage.ptr());
+	System::Drawing::Bitmap^ b = gcnew System::Drawing::Bitmap(colorImage.cols, colorImage.rows, colorImage.step, System::Drawing::Imaging::PixelFormat::Format24bppRgb, ptr);
+	System::Drawing::RectangleF rect(0, 0, control->Width, control->Height);
+	graphics->DrawImage(b, rect);
+	delete graphics;
+}
 
-	if (src.empty())
+
+
+/** @function to detect and display faces*/
+void detectAndDisplay(Mat frame, System::Windows::Forms::PictureBox^ pic)
+{
+	std::vector<Rect> faces; //initialize vector of found faces
+	Mat frame_gray;
+
+	//convert to grayscale opencv
+	cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
+	equalizeHist(frame_gray, frame_gray);
+
+	//-- Detect faces
+	face_cascade.detectMultiScale(frame_gray, faces, 1.1, 2, 0 | CASCADE_SCALE_IMAGE, Size(30, 30));
+	if (faces.size() > 0)
 	{
-		return -1;
+		face_detected = true;
 	}
+	else
+	{
+		face_detected = false;
+	}
+	/*
+	Rectangles in opencv
+	void rectangle(Mat& img, Point pt1, Point pt2, const Scalar& color, int thickness=1, int lineType=8, int shift=0)
+	Parameters:
+	img – Image.
+	pt1 – Vertex of the rectangle.
+	pt2 – Vertex of the rectangle opposite to pt1 .
+	rec – Alternative specification of the drawn rectangle.
+	color – Rectangle color or brightness (grayscale image).
+	thickness – Thickness of lines that make up the rectangle. Negative values, like CV_FILLED , mean that the function has to draw a filled rectangle.
+	lineType – Type of the line. See the line() description.
+	shift – Number of fractional bits in the point coordinates.
+	
+	NB: .tl() = TopLeft;
+		.br() = BottomRight;
+	*/
 
-	/// Create windows
-	namedWindow("Erosion Demo", WINDOW_AUTOSIZE);
-	namedWindow("Dilation Demo", WINDOW_AUTOSIZE);
-	moveWindow("Dilation Demo", src.cols, 0);
 
-	/// Create Erosion Trackbar
-	createTrackbar("Element:\n 0: Rect \n 1: Cross \n 2: Ellipse", "Erosion Demo",
-		&erosion_elem, max_elem,
-		Erosion);
+	/*
+	OpenCV Drawing a Circle
+	void circle(Mat& img, Point center, int radius, const Scalar& color, int thickness=1, int lineType=8, int shift=0)
+	Parameters:
+	img – Image where the circle is drawn.
+	center – Center of the circle.
+	radius – Radius of the circle.
+	color – Circle color.
+	thickness – Thickness of the circle outline, if positive. Negative thickness means that a filled circle is to be drawn.
+	lineType – Type of the circle boundary. See the line() description.
+	shift – Number of fractional bits in the coordinates of the center and in the radius value.
+	*/
 
-	createTrackbar("Kernel size:\n 2n +1", "Erosion Demo",
-		&erosion_size, max_kernel_size,
-		Erosion);
 
-	/// Create Dilation Trackbar
-	createTrackbar("Element:\n 0: Rect \n 1: Cross \n 2: Ellipse", "Dilation Demo",
-		&dilation_elem, max_elem,
-		Dilation);
+	for (size_t i = 0; i < faces.size(); i++) //draw rectangle around all faces
+	{
+		//-- Draw rectangle round the face
+		rectangle(frame, faces[i].br(), faces[i].tl(), Scalar(255, 255, 255), 3, 8, 0);
 
-	createTrackbar("Kernel size:\n 2n +1", "Dilation Demo",
-		&dilation_size, max_kernel_size,
-		Dilation);
+		Mat faceROI = frame_gray(faces[i]);
+		std::vector<Rect> eyes;
 
-	/// Default start
-	Erosion(0, 0);
-	Dilation(0, 0);
+		//haarcascade_frontalface_alt
 
-	waitKey(0);
+		//-- In each face, detect eyes 
+		eyes_cascade.detectMultiScale(faceROI, eyes, 1.1, 2, 0 | CASCADE_SCALE_IMAGE, Size(30, 30));
+
+		if (eyes.size() == 2) //two eyes found in face
+		{
+			for (size_t j = 0; j < eyes.size(); j++)
+			{ //-- Draw circle round the eyes
+				Point eye_center(faces[i].x + eyes[j].x + eyes[j].width / 2, faces[i].y + eyes[j].y + eyes[j].height / 2);
+				int radius = cvRound((eyes[j].width + eyes[j].height)*0.25);
+				circle(frame, eye_center, radius, Scalar(255, 0, 255), 3, 8, 0);
+			}
+		}
+	}
+	//-- Show what you got
+	//DrawCvImage(pic, frame);
+	imshow("Press Escape To Stop", frame);
+}
+
+
+
+//camera feed
+int video_cap(System::Windows::Forms::PictureBox^ pic, System::Windows::Forms::Control^ control)
+{	
+	VideoCapture cap;
+	if (!cap.open(0)) // open the default camera (camera 0), use something different from 0 otherwise;
+		return 0;
+	cap.set(CV_CAP_PROP_FRAME_WIDTH, 320);
+	cap.set(CV_CAP_PROP_FRAME_HEIGHT, 240);
+	while (1) {
+		double alpha = 2.0; /**< Simple contrast control (alpha value [1.0-3.0])*/ 
+		int beta = 2;  /**< Simple brightness control (beta value [0-100])*/
+		Mat frame;
+		cap >> frame;
+		if (frame.empty()) {}; // end of video stream
+		Mat new_image = Mat::zeros(frame.size(), frame.type());
+
+		/// Do the operation new_image(i,j) = alpha*image(i,j) + beta to increase image brightness
+		for (int y = 0; y < frame.rows; y++)
+		{
+			for (int x = 0; x < frame.cols; x++)
+			{
+				for (int c = 0; c < 3; c++)
+				{
+					new_image.at<Vec3b>(y, x)[c] =
+						saturate_cast<uchar>(alpha*(frame.at<Vec3b>(y, x)[c]) + beta);
+				}
+			}
+		}
+
+		detectAndDisplay(new_image, pic);
+		int c = waitKey(10);
+		if ((char)c == 27) { 
+			control->Text = "Start Detecting";
+			control->Enabled = true;
+			// close the window
+			face_detected = false;
+			destroyAllWindows();
+			break; 
+		} // escape
+	}
+	// the camera will be closed automatically upon exit
+	// cap.close();
 	return 0;
 }
 
 
-//![erosion]
-/**
-* @function Erosion
-*/
-void opencv_gui::Erosion(int, void*)
+
+
+
+int opencv_gui::button_test(System::Windows::Forms::PictureBox^ pic, System::Windows::Forms::Control^ control, System::Windows::Forms::TrackBar^ brightness, System::Windows::Forms::TrackBar^ contrast)
 {
-	int erosion_type = 0;
-	if (erosion_elem == 0) { erosion_type = MORPH_RECT; }
-	else if (erosion_elem == 1) { erosion_type = MORPH_CROSS; }
-	else if (erosion_elem == 2) { erosion_type = MORPH_ELLIPSE; }
+	brightness_val = brightness->Value;
+	contrast_val = contrast->Value;
+	//-- 1. Load the cascades for the facial identification
+	if (!face_cascade.load(face_cascade_name)) { printf("--(!)Error loading face cascade\n"); return -1; };
+	if (!eyes_cascade.load(eyes_cascade_name)) { printf("--(!)Error loading eyes cascade\n"); return -1; };
 
-	//![kernel]
-	Mat element = getStructuringElement(erosion_type,
-		cv::Size(2 * erosion_size + 1, 2 * erosion_size + 1),
-		cv::Point(erosion_size, erosion_size));
-	//![kernel]
-
-	/// Apply the erosion operation
-	erode(src, erosion_dst, element);
-	imshow("Erosion Demo", erosion_dst);
+	video_cap(pic, control);
+	return 0;
 }
-//![erosion]
 
-//![dilation]
-/**
-* @function Dilation
-*/
-void opencv_gui::Dilation(int, void*)
+
+bool opencv_gui::face_det(void)
 {
-	int dilation_type = 0;
-	if (dilation_elem == 0) { dilation_type = MORPH_RECT; }
-	else if (dilation_elem == 1) { dilation_type = MORPH_CROSS; }
-	else if (dilation_elem == 2) { dilation_type = MORPH_ELLIPSE; }
-
-	Mat element = getStructuringElement(dilation_type,
-		cv::Size(2 * dilation_size + 1, 2 * dilation_size + 1),
-		cv::Point(dilation_size, dilation_size));
-
-	/// Apply the dilation operation
-	dilate(src, dilation_dst, element);
-	imshow("Dilation Demo", dilation_dst);
+	return face_detected;
 }
-//![dilation]
+
+
+
+
 
--- opencv_gui/MyForm.h
+++ opencv_gui/MyForm.h
@@ -1,5 +1,12 @@
 #pragma once
 
+#include "opencv2/highgui.hpp"
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/opencv.hpp"
+
+
 namespace opencv_gui {
 
 	using namespace System;
@@ -8,13 +15,14 @@ namespace opencv_gui {
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
+	
+	bool stop_record = false;
 
 
 	/** Function Headers */
-	int button_test(void);
-	void Erosion(int, void*);
-	void Dilation(int, void*);
-
+	int button_test(System::Windows::Forms::PictureBox^, System::Windows::Forms::Control^, System::Windows::Forms::TrackBar^, System::Windows::Forms::TrackBar^);
+	bool face_det(void);
+	float millsecondsPerStep = 2000;
 
 	/// <summary>
 	/// Summary for MyForm
@@ -41,14 +49,36 @@ namespace opencv_gui {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::Button^  cvtest;
+	public: System::Windows::Forms::Button^  cvtest;
+	public: System::Windows::Forms::PictureBox^  pictureBox1;
+	private: System::Windows::Forms::Button^  stop;
+	private: System::Windows::Forms::Timer^  timer1;
+	private: System::Windows::Forms::Label^  label1;
+	public: System::Windows::Forms::PictureBox^  pictureBox2;
+	public: System::Windows::Forms::TrackBar^  brightness;
+	public:
+
+
+
+	private: System::Windows::Forms::Label^  label3;
+	public: System::Windows::Forms::TrackBar^  contrast;
+	private: System::Windows::Forms::Label^  label2;
+	private: System::Windows::Forms::Panel^  panel1;
+	private: System::Windows::Forms::Label^  label4;
+
+	public:
+
+	private: System::ComponentModel::IContainer^  components;
+	public:
+
+	public:
 	protected:
 
 	private:
 		/// <summary>
 		/// Required designer variable.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -57,35 +87,198 @@ namespace opencv_gui {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->components = (gcnew System::ComponentModel::Container());
 			this->cvtest = (gcnew System::Windows::Forms::Button());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->stop = (gcnew System::Windows::Forms::Button());
+			this->timer1 = (gcnew System::Windows::Forms::Timer(this->components));
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->pictureBox2 = (gcnew System::Windows::Forms::PictureBox());
+			this->brightness = (gcnew System::Windows::Forms::TrackBar());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->contrast = (gcnew System::Windows::Forms::TrackBar());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->panel1 = (gcnew System::Windows::Forms::Panel());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->brightness))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->contrast))->BeginInit();
+			this->panel1->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// cvtest
 			// 
-			this->cvtest->Location = System::Drawing::Point(27, 240);
+			this->cvtest->Location = System::Drawing::Point(26, 376);
 			this->cvtest->Name = L"cvtest";
-			this->cvtest->Size = System::Drawing::Size(107, 34);
+			this->cvtest->Size = System::Drawing::Size(104, 34);
 			this->cvtest->TabIndex = 0;
-			this->cvtest->Text = L"Test";
+			this->cvtest->Text = L"Start Detecting";
 			this->cvtest->UseVisualStyleBackColor = true;
 			this->cvtest->Click += gcnew System::EventHandler(this, &MyForm::cvtest_Click);
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->BackColor = System::Drawing::Color::Black;
+			this->pictureBox1->Location = System::Drawing::Point(26, 26);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(331, 313);
+			this->pictureBox1->TabIndex = 1;
+			this->pictureBox1->TabStop = false;
+			// 
+			// stop
+			// 
+			this->stop->Location = System::Drawing::Point(26, 501);
+			this->stop->Name = L"stop";
+			this->stop->Size = System::Drawing::Size(104, 34);
+			this->stop->TabIndex = 2;
+			this->stop->Text = L"Stop";
+			this->stop->UseVisualStyleBackColor = true;
+			this->stop->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// timer1
+			// 
+			this->timer1->Interval = 1;
+			this->timer1->Tick += gcnew System::EventHandler(this, &MyForm::timer1_Tick);
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9, System::Drawing::FontStyle::Bold));
+			this->label1->Location = System::Drawing::Point(23, 423);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(107, 15);
+			this->label1->TabIndex = 3;
+			this->label1->Text = L"Face Detected\?";
+			// 
+			// pictureBox2
+			// 
+			this->pictureBox2->BackColor = System::Drawing::Color::Red;
+			this->pictureBox2->Location = System::Drawing::Point(26, 441);
+			this->pictureBox2->Name = L"pictureBox2";
+			this->pictureBox2->Size = System::Drawing::Size(104, 43);
+			this->pictureBox2->TabIndex = 4;
+			this->pictureBox2->TabStop = false;
+			// 
+			// brightness
+			// 
+			this->brightness->Location = System::Drawing::Point(42, 140);
+			this->brightness->Maximum = 100;
+			this->brightness->Name = L"brightness";
+			this->brightness->Size = System::Drawing::Size(104, 45);
+			this->brightness->TabIndex = 6;
+			this->brightness->Value = 50;
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label3->Location = System::Drawing::Point(53, 118);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(81, 16);
+			this->label3->TabIndex = 8;
+			this->label3->Text = L"Brightness";
+			// 
+			// contrast
+			// 
+			this->contrast->LargeChange = 1;
+			this->contrast->Location = System::Drawing::Point(44, 64);
+			this->contrast->Maximum = 3;
+			this->contrast->Minimum = 1;
+			this->contrast->Name = L"contrast";
+			this->contrast->Size = System::Drawing::Size(104, 44);
+			this->contrast->TabIndex = 5;
+			this->contrast->Value = 2;
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label2->Location = System::Drawing::Point(65, 44);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(65, 16);
+			this->label2->TabIndex = 7;
+			this->label2->Text = L"Contrast";
+			// 
+			// panel1
+			// 
+			this->panel1->Controls->Add(this->contrast);
+			this->panel1->Controls->Add(this->brightness);
+			this->panel1->Controls->Add(this->label4);
+			this->panel1->Controls->Add(this->label2);
+			this->panel1->Controls->Add(this->label3);
+			this->panel1->Location = System::Drawing::Point(185, 376);
+			this->panel1->Name = L"panel1";
+			this->panel1->Size = System::Drawing::Size(198, 196);
+			this->panel1->TabIndex = 9;
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label4->Location = System::Drawing::Point(24, 6);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(148, 24);
+			this->label4->TabIndex = 10;
+			this->label4->Text = L"Video Controls";
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(508, 347);
+			this->ClientSize = System::Drawing::Size(730, 630);
+			this->Controls->Add(this->panel1);
+			this->Controls->Add(this->pictureBox2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->stop);
+			this->Controls->Add(this->pictureBox1);
 			this->Controls->Add(this->cvtest);
 			this->Name = L"MyForm";
 			this->Text = L"MyForm";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->brightness))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->contrast))->EndInit();
+			this->panel1->ResumeLayout(false);
+			this->panel1->PerformLayout();
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
-	private: System::Void cvtest_Click(System::Object^  sender, System::EventArgs^  e) {
-		button_test();
-	}
-	};
-}
 
+	private: System::Void cvtest_Click(System::Object^  sender, System::EventArgs^  e) {
+		if (cvtest->Text == "Detecting..") {
+			cvtest->Text = "Start Detecting";
+		}
+		else {
+			cvtest->Enabled = false;
+			cvtest->Text = "Detecting..";
+		}
+		stop_record = false;
+		timer1->Interval = millsecondsPerStep;
+		timer1->Enabled = true;
+		if (!stop_record) {
+		button_test(pictureBox1, cvtest, brightness, contrast);
+	}
+	}
+	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+		pictureBox2->BackColor = System::Drawing::Color::Green;
+		stop_record = true;
+		//timer1->Enabled = false;
+	}
+	private: System::Void timer1_Tick(System::Object^  sender, System::EventArgs^  e) {
+		if (face_det()) {
+		pictureBox2->BackColor = System::Drawing::Color::Green;
+	}
+		else
+		{
+			pictureBox2->BackColor = System::Drawing::Color::Red;
+		}
+}
+};
+}
 
